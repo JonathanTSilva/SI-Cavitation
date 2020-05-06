84138a453db857f48a1abf33af89e7a73dbc1140
Jonathan Tobias da Silva <58694273+JonathanTSilva@users.noreply.github.com>
2020-05-06 16:27:43 -0300

    Add files via upload

---
+++ PacketRawDataConversion.m
@@ -0,0 +1,34 @@
+%--------------------------------------------------------------------------
+% Script para conversão do vetor packetRawData[] gerado pela
+% pcap2mat tool (v.1.0.0 BETA)
+% Data: 2020-01-30
+%
+% Exemplo de uso:
+% packets = PacketRawDataConversion(packetRawData);
+%--------------------------------------------------------------------------
+
+function ret = PacketRawDataConversion(packetRawData)
+
+%Obtém tamanho do vetor
+tam = size(packetRawData);
+
+%Converte os dados separados por , em array individuais
+tam = tam(1);
+for i = 1:tam
+    packetRawDataS{i} = strsplit(packetRawData{i},',');
+end;
+
+clear packetRawData;
+
+%Converte os valores para inteiro (facilitar o processamento dos bytes)
+for i = 1:tam
+    packetRawData{i} = uint8(hex2dec(packetRawDataS{i}));
+end;
+
+%faz a tranposição do vetor
+packetRawData = packetRawData';
+
+%apaga variáveis auxiliares da memória
+clear packetRawDataS tam i;
+
+ret = packetRawData;
