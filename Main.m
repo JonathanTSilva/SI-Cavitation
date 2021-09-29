843adac0ee0257ea628f14c28b7714d0f9113e99
Jonathan Tobias da Silva <58694273+JonathanTSilva@users.noreply.github.com>
2021-09-29 18:11:48 -0300

    Update to read datased
    Fix #1

--- Main.m
+++ Main.m
@@ -1,40 +1,40 @@
 %-------------------------JONATHAN TOBIAS DA SILVA-------------------------
-%----------------------ENGENHARIA ELÉTRICA - 1ª TURMA----------------------
-%------------------INSTITUTO FEDERAL- CAMPUS SERTÃOZINHO-------------------
+%----------------------ENGENHARIA ELÃ‰TRICA - 1Âª TURMA----------------------
+%------------------INSTITUTO FEDERAL- CAMPUS SERTÃƒOZINHO-------------------
 %--------------------------------------------------------------------------
-%--------------------INICIAÇÃO - DIAGNÓSTICO DE FALHAS---------------------
+%--------------------INICIAÃ‡ÃƒO - DIAGNÃ“STICO DE FALHAS---------------------
 %--------------------------------------------------------------------------
 
 clear all; close all; clc;
 
-% Variáveis
+% VariÃ¡veis
 j = 1;
 k = 1;
 
 %1- Carregar banco de dados:
-fileName = "x";
+fileName = "normal";
 file = sprintf("%s.mat", fileName);
-folder = "..\Dados\Oficial\mat\116cm\";
+folder = "..\Dataset\";
 dir = strcat(folder,file);
 load(dir);
 
-%2- Convertê-lo para arrays:
+%2- ConvertÃª-lo para arrays:
 packetRawData = PacketRawDataConversion(packetRawData);
 
-%3- Coletar dados das correntes e tensões, enviadas pelo Simocode:
+%3- Coletar dados das correntes e tensÃµes, enviadas pelo Simocode:
     %Verificar qual a origem:
         % Se a origem for do CLP - Coletar SP (Setpoint de Corrente)
-        % Se a origem for do Simocode - Coletar Im (Corrente Máxima)
+        % Se a origem for do Simocode - Coletar Im (Corrente MÃ¡xima)
         % Como complemento: I1, I2, I3 e V1, V2, V3;
 for i=1:length(packetRawData)
-    %1 Byte responsável por dizer a origem do pacote:
+    %1 Byte responsÃ¡vel por dizer a origem do pacote:
     frame = packetRawData{i}(12);
-    %Quando o byte 12 for igual a #FEh, o pacote é oriundo do SIMOCODE
+    %Quando o byte 12 for igual a #FEh, o pacote Ã© oriundo do SIMOCODE
     if (frame == 200)
         Imax(j) = GetActualCurrent(packetRawData{i}(23),packetRawData{i}(24));
         j = j + 1;
     end
-   %Quando o byte 12 for igual a #C8h, o pacote é oriundo do CLP
+   %Quando o byte 12 for igual a #C8h, o pacote Ã© oriundo do CLP
 %    if (frame == 254)
 %        k = k + 1;
 %    end
@@ -43,15 +43,15 @@ end
 %4- Dividir amostras:
 aSplitSamples;
 
-%5- Extrair atributos e gerar harmônicas:
+%5- Extrair atributos e gerar harmÃ´nicas:
 aAttributesExtraction;
 
 %6- Exportar attributes para csv:
 csvName = sprintf("%s.csv",fileName);
 csvwrite(csvName,attributes,0);
 
-%- Gerar gráficos:
+%- Gerar grÃ¡ficos:
 aGenerateFigures;
 
-%Apagar variáveis:
+%Apagar variÃ¡veis:
 clearvars file folder dir folderFig fig1 fig1Name fig1Local fig2 fig2Name fig2Local;
